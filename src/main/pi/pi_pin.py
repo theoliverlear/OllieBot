5eb242bd4692ac5b8f382ac99c43f4414d865e51
theoliverlear <sigwartholiver2@gmail.com>
2024-09-23 18:33:38 -0500

    Add default pin state, begin state queuing, and state validation methods in pi_pin.py

--- src/main/pi/pi_pin.py
+++ src/main/pi/pi_pin.py
@@ -2,7 +2,7 @@ from src.main.pi.pin_state import PinState
 
 
 class PiPin:
-    def __init__(self, pin_number: int, pin_state: PinState):
+    def __init__(self, pin_number: int, pin_state: PinState = PinState.DISABLED):
         self._pin_number = pin_number
         self.pin_state = pin_state
 
@@ -10,13 +10,23 @@ class PiPin:
     def pin_number(self):
         return self._pin_number
 
+    def enqueue_state(self, pin_state: PinState):
+        pass
+
+    def prime_state(self, pin_state: PinState):
+        if self.is_new_state(pin_state):
+            self.pin_state = pin_state
+
     def set_state(self, pin_state: PinState):
-        if not self.pin_state == pin_state:
+        if self.is_new_state(pin_state):
             self.pin_state = pin_state
             self.execute_state()
 
     def execute_state(self):
         GPIO.output(self.pin_number, self.pin_state.value)
+
+    def is_new_state(self, pin_state: PinState) -> bool:
+        return not self.pin_state == pin_state
 
     def toggle_state(self):
         if self.pin_state == PinState.ENABLED:
