a43ac285214e10080650cb15a74af4c410001815
theoliverlear <sigwartholiver2@gmail.com>
2024-09-23 18:33:06 -0500

    Change attribute access in PiController to retrieve pins from pi_pins in pi_controller.py

--- src/main/pi/pi_controller.py
+++ src/main/pi/pi_controller.py
@@ -7,7 +7,7 @@ from src.main.robot.robot import Robot
 class PiController:
     def __init__(self, pi_config: PiConfig):
         self.pi_config = pi_config
-        self.pi_pins = PiPins(pi_config.pins)
+        self.pi_pins = PiPins(pi_config.pi_pins.pins)
 
     def apply_config(self):
         pass
