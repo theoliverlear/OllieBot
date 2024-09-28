d1c39a48426314751cc014cef157c663203edc17
theoliverlear <sigwartholiver2@gmail.com>
2024-09-28 17:26:42 -0500

    Add Light control in main.py to demonstrate on/off functionality

--- src/main/main.py
+++ src/main/main.py
@@ -3,21 +3,22 @@ import time
 
 from src.main.pi.pi_pin import PiPin
 from src.main.pi.pin_state import PinState
+from src.main.robot.component.light.light import Light
 from src.main.robot.robot import Robot
-import gpiozero
+
 class Main:
-    robot: Robot
     def __init__(self, robot):
         self.robot = robot
-        pass
     @staticmethod
     def main():
         robot: Robot = Robot.default_robot()
-        robot.pi_controller.pi_pins.set_pin(PiPin(11, PinState.DISABLED))
-        robot.pi_controller.pi_pins.get_pin_by_pin_number(11).set_state(PinState.ENABLED)
-        time.sleep(1)
-        robot.pi_controller.pi_pins.get_pin_by_pin_number(11).set_state(PinState.DISABLED)
-if __name__ == '__main__':
+        light: Light = Light(PiPin(17, PinState.DISABLED))
+        light.on()
+        time.sleep(2)
+        light.off()
+
+if __name__ == "__main__":
     Main.main()
-    # video_recorder = VideoRecorder.create_from_defaults()
+    print("Done")
+# video_recorder = VideoRecorder.create_from_defaults()
     # video_recorder.record()
