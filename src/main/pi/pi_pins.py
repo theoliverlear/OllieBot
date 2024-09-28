8d54f38bbcace0d728e7a594eed31f83cd88e186
theoliverlear <sigwartholiver2@gmail.com>
2024-09-28 08:05:23 -0500

    Add GPIO_PINS constant and hard_set_pin method in pi_pins.py to manage PiPin configurations

--- src/main/pi/pi_pins.py
+++ src/main/pi/pi_pins.py
@@ -6,6 +6,7 @@ from src.main.pi.pi_pin import PiPin
 
 
 class PiPins:
+    GPIO_PINS = [3, 5, 7, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36, 37, 38, 40]
     def __init__(self, pins: List[PiPin]):
         self.pins = pins
         self.detect_overlapping_pins()
@@ -40,3 +41,10 @@ class PiPins:
                 else:
                     pin = new_pi_pin
                     return
+
+    def hard_set_pin(self, new_pi_pin: PiPin) -> None:
+        for i in range(len(self.pins)):
+            if self.pins[i].pin_number == new_pi_pin.pin_number:
+                self.pins[i] = new_pi_pin
+                return
+        self.pins.append(new_pi_pin)
