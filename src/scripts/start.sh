dd1532d11a90ddf2b72add0dc374ee2babba667e
theoliverlear <sigwartholiver2@gmail.com>
2024-09-28 17:27:57 -0500

    Add start and stop messages to start.sh for better process visibility

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 source /home/ludwig/GitHub/OllieBot/environment/bin/activate
 export PYTHONPATH="/home/ludwig/GitHub/OllieBot"
+echo "Starting OllieBot"
 PYTHONPATH=$PYTHONPATH /home/ludwig/GitHub/OllieBot/environment/bin/python3 /home/ludwig/GitHub/OllieBot/src/main/main.py
+echo "OllieBot has stopped"
