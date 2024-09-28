6d5017307d4a9887276ccaa30af6d070b8fae636
theoliverlear <sigwartholiver2@gmail.com>
2024-09-28 08:06:23 -0500

    Add startup script start.sh to activate virtual environment and execute main.py

---
+++ src/scripts/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /home/ludwig/GitHub/OllieBot/environment/bin/activate
+export PYTHONPATH="/home/ludwig/GitHub/OllieBot"
+PYTHONPATH=$PYTHONPATH /home/ludwig/GitHub/OllieBot/environment/bin/python3 /home/ludwig/GitHub/OllieBot/src/main/main.py
