ba097d07947413a8f501e428eb74bafe93550eeb
theoliverlear <sigwartholiver2@gmail.com>
2024-09-28 17:27:20 -0500

    Add traceback printing on OverlappingPinException in overlapping_pin_exception.py

--- src/main/pi/exception/overlapping_pin_exception.py
+++ src/main/pi/exception/overlapping_pin_exception.py
@@ -1,8 +1,11 @@
 import sys
+import traceback
 
 
 class OverlappingPinException(Exception):
     def __init__(self, pin_number: int):
         message: str = f"Pin number {pin_number} is already in use."
         super().__init__(message)
+        print(message)
+        traceback.print_exc()
         sys.exit(1)
